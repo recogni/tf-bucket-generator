1e87739df277282c37895f9795d6ecc98bb9bf8f
Eugene Feinberg <eugene@recogni.com>
2019-03-29 14:55:40 -0700

    python3 fixes

--- tf_packet_generator/generator.py
+++ tf_packet_generator/generator.py
@@ -7,6 +7,7 @@ import time
 from tensorflow.python.lib.io import file_io
 from recogni_proto import v0_packet_pb2 as packet
 
+
 class CameraPacketGenerator():
     """ Generator class to wrap the recogni specific camera packet
         generator.
@@ -21,17 +22,16 @@ class CameraPacketGenerator():
         for pattern in patterns:
             self.patterns.append(pattern)
 
-
     def generator(self):
         """ generator function for the dataset.  This will yield a single
             recogni v0_packet_pb2.CameraPacket at a time.
         """
         bundle = packet.Bundle()
         for pattern in self.patterns:
-            print "Using pattern: ", pattern
+            print("Using pattern: ", pattern)
             files = file_io.get_matching_files(pattern)
             for file in files:
-                print "... file ", file
+                print("... file ", file)
                 with file_io.FileIO(file, "rb") as fin:
                     bs = fin.read()
                     try:
