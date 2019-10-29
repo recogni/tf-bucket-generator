c9bbdda494179a74c2bfc8a68445b33e15126be1
Berend Ozceri <berend@recogni.com>
2019-10-29 10:42:49 +0100

    Update version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="tf_packet_generator",
-    version=0.1,
+    version="0.1.1",
     description="Tensorflow data generator for recogni CameraPackets",
     url="https://github.com/recogni/tf-bucket-generator",
     author="sabhiram",
